5bce3ba4af0fd46af68285987575adddd2a089b3
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2026-04-16 01:29:10 -0400

    re-arrange network.tf

--- infra-epicbook/network.tf
+++ infra-epicbook/network.tf
@@ -1,15 +1,22 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = "epicbook-vnet"
-  location            = var.location
-  resource_group_name = var.rg_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_public_ip" "app" {
-  name                = "app-public-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
+resource "azurerm_subnet" "app_subnet" {
+  name                 = "app-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "db_subnet" {
+  name                 = "db-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "app_nic" {
@@ -37,16 +44,10 @@ resource "azurerm_network_interface" "db_nic" {
   }
 }
 
-resource "azurerm_subnet" "app" {
-  name                 = "app-subnet"
-  resource_group_name  = var.rg_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+resource "azurerm_public_ip" "app" {
+  name                = "app-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
 }
 
-resource "azurerm_subnet" "db" {
-  name                 = "db-subnet"
-  resource_group_name  = var.rg_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
