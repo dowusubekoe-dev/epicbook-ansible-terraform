b992f70d73aa9e6f2ac012a28e4bad125768edae
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2026-04-17 15:04:10 -0400

    SKU resolved

--- infra-epicbook/variables.tf
+++ infra-epicbook/variables.tf
@@ -35,28 +35,18 @@ variable "ssh_public_key" {
 
 variable "vm_size" {
   type    = string
-  default = "Standard_B2s_v2"
-
-validation {
-    condition = contains([
-      "Standard_B2ls_v2",
-      "Standard_B2s_v2",
-      "Standard_B2ts_v2"
-    ], var.vm_size)
-
-    error_message = "Only B-series v2 sizes are allowed in this environment."
-  }
+  default = "Standard_B2ts_v2"
 }
 
 variable "location" {
   type    = string
-  default = "westeurope"
+  default = "northeurope"
 
   validation {
     condition = contains([
-      "westeurope"
+      "northeurope"
     ], var.location)
 
-    error_message = "Only westeurope is allowed in this environment."
+    error_message = "Only northeurope is allowed in this environment."
   }
 }
